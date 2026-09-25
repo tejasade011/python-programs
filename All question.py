73cfe7c46a7915bc718bac87465e0e560f8673a6
Mr.Tejas Ade <tejasade700@gmail.com>
2026-09-25 12:33:44 +0530

    Update All question.py

--- All question.py
+++ All question.py
@@ -6,12 +6,26 @@ name = input("Enter your name:tejas ")
 age = input("Enter your age:18 ")
 college = input("Enter your college name:RBU ")
 branch = input("Enter your branch:ECS ")
-
-# Printing the collected information
 print("\n--- Student Details ---")
 print(f"Name:    {name}")
 print(f"Age:     {age}")
 print(f"College: {college}")
 print(f"Branch:  {branch}")
+
+
+
+Q2.Take name as input and greet the user
+
+name=input("hello!tejas")
+print(f"hello,{name}!It's nice to meet you")
+
+
+
+Q3.Take name as input and greet the user
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+result = num1 + num2
+print(f"The sum of {num1} and {num2} is {result}")
 
 
