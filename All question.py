35d44b834c4bcbd3060848a464e9d0691c6cb997
Mr.Tejas Ade <tejasade700@gmail.com>
2026-09-25 12:44:53 +0530

    Update All question.py

--- All question.py
+++ All question.py
@@ -65,5 +65,13 @@ print("Simple Interest:", si)
 
 
 
+Q7.Convert Celsius to Fahrenheit
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print("Temperature in Fahrenheit:", fahrenheit)
+
+
+
 
 
