3a09ea72eaf360cc7a7a91a60fa918e7513d0594
Mr.Tejas Ade <tejasade700@gmail.com>
2026-09-26 10:57:47 +0530

    Add script to calculate total and percentage of marks
    This script calculates the total and percentage of marks for five subjects based on user input.

---
+++ Q8.py
@@ -0,0 +1,11 @@
+Q8.Calculate total and percentage of 5 subjects
+
+s1 = float(input("Enter marks of subject 1: "))
+s2 = float(input("Enter marks of subject 2: "))
+s3 = float(input("Enter marks of subject 3: "))
+s4 = float(input("Enter marks of subject 4: "))
+s5 = float(input("Enter marks of subject 5: "))
+total = s1 + s2 + s3 + s4 + s5
+percentage = total / 5
+print("Total:", total)
+print("Percentage:", percentage, "%")
