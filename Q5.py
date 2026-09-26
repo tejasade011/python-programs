b16b876ef7f642516c8a6bbcc8d875adbcbd2fc7
Mr.Tejas Ade <tejasade700@gmail.com>
2026-09-26 10:55:06 +0530

    Update Q5.py

--- Q5.py
+++ Q5.py
@@ -1,14 +1,6 @@
-Q4.Perform all arithmetic operations on two numbers
+Q5.Calculate area of a circle
 
-num1=float(intput("enter the number 1")
-num2=float(intput("enter the number 2")
-addition       = num1 + num2
-subtraction    = num1 - num2
-multiplication = num1 * num2
-division       = num1 / num2
-# Display the results
-print(f"Numbers: {num1} and {num2}\n")
-print(f"Addition (+):          {addition}")
-print(f"Subtraction (-):       {subtraction}")
-print(f"Multiplication (*):    {multiplication}")
-print(f"Division (/):          {division}")
+import math
+radius = float(input("Enter radius: "))
+area =3.14*pi * radius * radius
+print("Area of circle:", area)
