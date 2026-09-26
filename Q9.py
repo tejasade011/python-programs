c9e61e53b49f2f15ddde1de52524b5ce5ea92ebf
Mr.Tejas Ade <tejasade700@gmail.com>
2026-09-26 10:58:46 +0530

    Add a script to swap two numbers

---
+++ Q9.py
@@ -0,0 +1,8 @@
+Q9.Swap two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+a, b = b, a
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
