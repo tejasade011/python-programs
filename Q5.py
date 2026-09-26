7a9d25417230bdfca8a7a46756f2b58010e2acc1
Mr.Tejas Ade <tejasade700@gmail.com>
2026-09-26 10:52:56 +0530

    Create Q5.py

---
+++ Q5.py
@@ -0,0 +1,14 @@
+Q4.Perform all arithmetic operations on two numbers
+
+num1=float(intput("enter the number 1")
+num2=float(intput("enter the number 2")
+addition       = num1 + num2
+subtraction    = num1 - num2
+multiplication = num1 * num2
+division       = num1 / num2
+# Display the results
+print(f"Numbers: {num1} and {num2}\n")
+print(f"Addition (+):          {addition}")
+print(f"Subtraction (-):       {subtraction}")
+print(f"Multiplication (*):    {multiplication}")
+print(f"Division (/):          {division}")
